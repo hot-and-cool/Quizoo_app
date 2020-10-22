a593dbea1a739c312540fe84cd683cd46e393e98
hst-take <hisato0217take@gmail.com>
2020-10-22 16:48:05 +0900

    add:ホーム画面

--- server/quizoo/webpack.mix.js
+++ server/quizoo/webpack.mix.js
@@ -11,12 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/home.js', 'public/js')
+  .js('resources/js/quiz.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css', {
+    implementation: require('node-sass')
+  });
 
 mix.browserSync({
-    files: [
-        "public/**/*.*"
-    ],
-    proxy: "http://localhost:8070"
+  files: [
+    "public/**/*.*"
+  ],
+  proxy: "http://localhost:8070",
 });
